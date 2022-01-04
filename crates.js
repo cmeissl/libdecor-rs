a117b0d9ad4542c15610a3517dd0136c31333a05
cmeissl <cmeissl@users.noreply.github.com>
2022-01-04 09:46:04 +0000

    deploy: fb6724563c89a8a0a13f81d201bfc9d8991a7806

---
+++ crates.js
@@ -0,0 +1,1 @@
+window.ALL_CRATES = ["libdecor","libdecor_sys"];
---
+++ source-files.js
@@ -0,0 +1,4 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["libdecor"] = {"name":"","files":["context.rs","frame.rs","lib.rs"]};
+sourcesIndex["libdecor_sys"] = {"name":"","files":["lib.rs"]};
+createSourceSidebar();
